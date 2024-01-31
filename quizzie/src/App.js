d561dc8d56c6cfe2e67e38ebd31f296dca09ca6b
Amit-kumar-verma <97842674+akv2511@users.noreply.github.com>
2024-02-01 01:42:07 +0530

    Update App.js

--- quizzie/src/App.js
+++ quizzie/src/App.js
@@ -5,8 +5,8 @@ import QuestionAnswerPage from "./Components/QuizInterface/QuestionAnswerPage";
 import DashboardHome from "./Components/QuizzieDashboard/DashboardHome";
 import Home from "./Components/QuizzieHome/Home";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
-// axios.defaults.baseURL = "http://it-amitverma-server.vercel.app/api";
-// axios.defaults.withCredentials = true;
+ axios.defaults.baseURL = "https://it-amitverma-server.vercel.app/api";
+axios.defaults.withCredentials = true;
 function App() {
   return (
     <BrowserRouter>
