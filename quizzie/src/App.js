db7587b465c6bcd91a4c5856b16ed00b38e4c887
Amit-kumar-verma <97842674+akv2511@users.noreply.github.com>
2024-01-31 22:43:26 +0530

    Update App.js

--- quizzie/src/App.js
+++ quizzie/src/App.js
@@ -5,7 +5,7 @@ import QuestionAnswerPage from "./Components/QuizInterface/QuestionAnswerPage";
 import DashboardHome from "./Components/QuizzieDashboard/DashboardHome";
 import Home from "./Components/QuizzieHome/Home";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
-axios.defaults.baseURL = "https://musicart-server.vercel.app/api";
+axios.defaults.baseURL = "it-amitverma-server.vercel.app";
 axios.defaults.withCredentials = true;
 function App() {
   return (
