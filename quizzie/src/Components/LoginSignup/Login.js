a3f85e25b70d3c13e38e6154d314cfa9f7bc26e0
Amit-kumar-verma <97842674+akv2511@users.noreply.github.com>
2024-02-01 11:05:36 +0530

    Add files via upload

--- quizzie/src/Components/LoginSignup/Login.js
+++ quizzie/src/Components/LoginSignup/Login.js
@@ -1,7 +1,7 @@
 import styles from './Login.module.css';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+
 function Login() {
     const navigate = useNavigate();
     const [email, setEmail] = useState("");
@@ -12,28 +12,38 @@ function Login() {
     const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
     function validateEmail(email) {
         return emailRegex.test(email);
-    }
+    }   
     const handelSubmit = async (event) => {
         event.preventDefault();
         if (!validateEmail(email) || password.length === 0) {
             setError(true);
-        } else {
-            try {
+        }else  {             
+            try  {
                 console.log("handleSignIn")
-                const response = await axios.post("/loginUser", { email, password });
-                    // .then((res) => res.json())
-                    // .then((respose) => {
-                    //     console.log(respose, "user-Login")
-                    //     if (respose.status === "SUCCESS") {
-                    //         localStorage.setItem("token", respose.jwtToken);
-                    //         localStorage.setItem("email", respose.email);
-                    //         navigate('/dashboardhome')
-                    //     } else {
-                    //         setApiError(respose)
-                    //     }
-                    // })
-            } 
-            catch (error) {
+            const response  =  await fetch('http://localhost:4000/api/loginUser', {
+                method: 'POST',
+                crossDomain: true,
+                headers: {
+                    'Content-Type': 'application/json',
+                    Accept: "application/json",
+                    "Access-Control-Allow-Origin": "*",
+                }, body: JSON.stringify({
+                    email,
+                    password,
+                }),
+                })
+                    .then((res) => res.json())
+                    .then((respose) => {
+                        console.log(respose, "user-Login")
+                        if (respose.status === "SUCCESS") {                            
+                            localStorage.setItem("token", respose.jwtToken);
+                            localStorage.setItem("email", respose.email);
+                            navigate('/dashboardhome')
+                        }else{
+                            setApiError(respose)
+                        }
+                    })                
+            } catch (error) {
                 console.log(error)
                 console.error('Error error login:', error.message);
                 setResponse('Error login user. Please try again.');
@@ -46,29 +56,29 @@ function Login() {
             <form onSubmit={handelSubmit} className={styles.loginform}>
                 <h1 className={styles.texth1}>
                     Email <input className={styles.logininput}
-                        type="email" name="email"
-                        placeholder='Enter your Register email-Id'
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
+                    type="email" name="email"
+                    placeholder='Enter your Register email-Id'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
                 </h1>
                 {error && !validateEmail(email) ?
                     <lable className={styles.error}>Invalid email address</lable> : ""}
                 <br />
                 <h1 className={styles.texth1}>
                     Password<input className={styles.logininput}
-                        type="password" name="password"
-                        placeholder="Enter your password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
+                    type="password" name="password"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
                 </h1 >
-
+                
                 {error && password.length <= 0 ?
                     <lable className={styles.error}>Can't be empty</lable> : ""}
                 <br />
                 <button onClick={handelSubmit} className={styles.loginbutn}>Log In</button>
-                {apiError &&
+                {apiError && 
                     <lable className={styles.error}>{apiError.message}</lable>}
             </form>
         </>
